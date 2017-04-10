2dfdc6149a4b47905e33cb5f85496e2968b78d59
Abhishek S <abhisheksms97@gmail.com>
2017-04-10 17:14:07 +0530

    Need to work on generating listview  via swipe transition animation

--- app/src/main/java/com/example/android/offerprototype/VerticalViewPager.java
+++ app/src/main/java/com/example/android/offerprototype/VerticalViewPager.java
@@ -2,7 +2,6 @@ package com.example.android.offerprototype;
 
 import android.view.View;
 import android.view.animation.Interpolator;
-
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
